7950863133ede0416e0b358903f7a8d3f8d80a9f
Abdullah al Masum <abdullahalmasum1120@gmail.com>
2022-01-05 22:28:01 +0600

    blocobserver added successfully to app

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:blood_donation/logic/blocs/app_bloc/app_bloc.dart';
+import 'package:blood_donation/logic/obserber/observer.dart';
 import 'package:blood_donation/ui/authentication/authentication.dart';
 import 'package:blood_donation/ui/home/home.dart';
 import 'package:blood_donation/ui/update_user_info/update_user_info.dart';
@@ -7,24 +8,26 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'logic/blocs/auth_bloc/auth_bloc.dart';
-import 'logic/obserber/observer.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  final Observer _blocObserver = Observer();
   final AuthBloc _authBloc = AuthBloc(FirebaseAuth.instance);
-
-  runApp(
-    MultiBlocProvider(
-      providers: [
-        BlocProvider(
-          create: (context) => AppBloc(_authBloc)..add(AppStartedEvent()),
+  BlocOverrides.runZoned(
+    () {
+      runApp(
+        MultiBlocProvider(
+          providers: [
+            BlocProvider(
+              create: (context) => AppBloc(_authBloc)..add(AppStartedEvent()),
+            ),
+            BlocProvider(create: (context) => _authBloc),
+          ],
+          child: MyApp(),
         ),
-        BlocProvider(create: (context) => _authBloc),
-      ],
-      child: MyApp(),
-    ),
+      );
+    },
+    blocObserver: Observer(),
   );
 }
 
